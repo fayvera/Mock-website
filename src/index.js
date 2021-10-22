cfc677987639ecc0ac75d36e715d8f04e8ee6b3b
Fay Vera <fayverawright@gmail.com>
2021-10-21 22:56:46 -0400

    connects with store

--- src/index.js
+++ src/index.js
@@ -2,16 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom'
+// import { createStore, compose, applyMiddleware } from 'redux';
+// import {composeWithDevTools} from 'redux-devtools-extension'
+// import thunk from 'redux-thunk';
+// import reducer from './reducers/index'
+
+// const store = createStore(reducer, compose(applyMiddleware(thunk), composeWithDevTools()))
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // <Provider store={store}>
+  <Provider>
+    <Router > 
+      <App />
+    </Router>
+  </Provider>,
   document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
