407b92e5de8ed6b63ef2988ff7bad5685666dced
Fay Vera <fayverawright@gmail.com>
2021-10-21 22:57:19 -0400

    creates navbar class

---
+++ src/Navbar/navbar.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+
+class Navbar extends Component{ 
+
+
+    render(){
+        return(
+            <nav class="navbar">
+                
+            </nav>
+        )
+    }
+}
+
+export default Navbar;
+
+
