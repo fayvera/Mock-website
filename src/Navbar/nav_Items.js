c4add8289e28e40a963bcfa8c9fd1008292b4c8f
Fay Vera <fayverawright@gmail.com>
2021-10-24 18:43:25 -0400

    ahandles more items in navbar

---
+++ src/Navbar/nav_Items.js
@@ -0,0 +1,17 @@
+export const items = [
+    {
+        title: "Email",
+        url: "/email",
+        cname: 'svg-links'
+    },
+    {
+        title: "GitHub",
+        url: "/github",
+        cname: "svg-links"
+    },
+    {
+        title: "LinkedIn",
+        url: "/linkedin",
+        cname: 'svg-links'
+    }
+]
--- src/Navbar/navbar.js
+++ src/Navbar/navbar.js
@@ -1,16 +1,45 @@
 import React, {Component} from 'react';
 import './navbar.css'
+import {items} from './nav_Items'
+import { Link } from "react-router-dom";
 
 
 class Navbar extends Component{ 
+    constructor(){
+        super();
+        this.state = {
+            clicked: false
+        }
+    }
+
+    handleClick = () => {
+        this.setState({ clicked: !this.state.clicked })
+    }
+
+
+    handleMenu = () => {
+        {items.map((item, index) => {
+            <src key={index} onClick={this.handleClick}>
+                <Link classname={item.cname} to={item.url}>
+                    {item.title}
+                </Link>
+            </src>
+        }
+    )}
+}
 
 
     render(){
         return(
             <nav class="navbar">
-                <h1>Home</h1>
-                <h1>About</h1>
-                <h1>Logout</h1>
+                <div class="navbar-main">
+                    <a>Home</a>
+                    <a>About</a>
+                    <a>Contact</a>
+                </div>
+                <div class="navbar-items">
+                    {this.handleMenu()}
+                </div>
             </nav>
         )
     }
