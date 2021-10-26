f3021f96290d1a321e9c98b40424806319d419e6
Fay Vera <fayverawright@gmail.com>
2021-10-26 10:16:09 -0400

    adds css file for about page

--- src/About/about.js
+++ src/About/about.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router';
+import '../stylesheets/about.css'
 // import {connect} from 'react-redux'
 // import { withRouter } from "react-router";
 
@@ -16,11 +17,18 @@ class About extends Component{
         return(
             <div className="main-about-div">
                 <h1>About Me!</h1>
+                <p id="about-me">
+                    <h3>Ruby </h3>
+                        <h3>Ruby on Rails</h3>
+                    <h3>React</h3>
+                    <h3>Redux</h3>
+                    <h3>HTML</h3>
+                    <h3>CSS</h3>                     
+                </p>    
             </div>
         )
     }
 }
 
-// export default withRouter(connect(null)(About));
+
 export default withRouter(About);
-// withRouter(About);
