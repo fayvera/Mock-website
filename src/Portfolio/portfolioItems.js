77c2f98e0b2b8e81c3fbeddb7297998184b05fdb
Fay Vera <fayverawright@gmail.com>
2021-10-30 19:51:10 -0400

    adds all project info

--- src/Portfolio/portfolioItems.js
+++ src/Portfolio/portfolioItems.js
@@ -1,11 +1,26 @@
 export const projects = [
     {
         title: "Brewery Locator",
-        description: "Brewery Locator where users can search beer-related establishments by location.",
+        description: "Brewery Locator where users can search beer-related establishments by location",
         picture: "img",
         githubLink: "https://github.com/fayvera/brews-out-there"
     },
     {
-        
+        title: "Fluent App",
+        description: "Platform intended to connect users for the purpose of practicing their language of choice",
+        picture: 'img',
+        githubLink: "https://github.com/fayvera/FluentApp"
+    },
+    {
+        title: "Hogwarts Sorting Quiz",
+        description: "A single page app that sorts users into houses from the Harry Potter series",
+        picture: 'img',
+        githubLink: "https://github.com/fayvera/harrypotter-js"
+    }, 
+    {
+        title: "Covid-19 Tracker",
+        description: "Tracker for the number of daily cases of COVID-19 in the 5 boroughs of NYC from Feb - Dec 2020",
+        picture: 'img',
+        githubLink: "https://github.com/fayvera/covid-app"
     }
 ]
