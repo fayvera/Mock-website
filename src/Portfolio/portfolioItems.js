d874201d05374d55b3b4dbf078a28ad85942f93a
Fay Vera <fayverawright@gmail.com>
2021-10-30 19:48:02 -0400

    creates file for projects

---
+++ src/Portfolio/portfolioItems.js
@@ -0,0 +1,11 @@
+export const projects = [
+    {
+        title: "Brewery Locator",
+        description: "Brewery Locator where users can search beer-related establishments by location.",
+        picture: "img",
+        githubLink: "https://github.com/fayvera/brews-out-there"
+    },
+    {
+        
+    }
+]
